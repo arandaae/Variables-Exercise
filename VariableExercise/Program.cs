5eedddbcb071c3a7ecb24664b3159ed7c48300b8
Armando Aranda <arandaae19@gmail.com>
2022-04-21 15:15:13 -0500

    first commit

---
+++ VariableExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace VariableExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Declare and initialize variables for the following types
+            string name = "Armando";
+            int myAge = 30;
+            char firstLetterOfLastName = 'A';
+            bool completedExercise = true;
+            double testDouble = 1.1;
+            decimal testDecimal = 2.2M;
+
+            Console.WriteLine($"My name is {name}, my age is {myAge} years old.");
+
+            Console.WriteLine($"The first letter of my last name is {firstLetterOfLastName} .");
+
+            Console.WriteLine($"This is an example of a double {testDouble}.");
+
+            Console.WriteLine($"This is an example of a decimal {testDecimal}.");
+
+            Console.WriteLine("I have completed this exercise");
+            Console.WriteLine(completedExercise);
+            
+
+            
+            
+        }
+    }
+}
